c78b0e87e32e62aa02b878630ab6c9eab1213e83
Flight Club 502 <flightclub502@gmail.com>
2023-11-16 10:31:00 -0500

    Added windows error noise on unknown member login and added comments to login_tracker

--- login_tracker.py
+++ login_tracker.py
@@ -2,6 +2,7 @@ import functools
 import os
 import sys
 import tkinter as tk
+import winsound
 import customtkinter as ctk
 from datetime import datetime, timedelta
 from tkinter import ttk
@@ -15,13 +16,17 @@ from ConfigHandler import ConfigHandler
 from LogWindow import LogWindow
 
 
-# from breeze import breeze
-
 def open_log_window():
-    log_window = LogWindow(root, "data/login_log.csv")
+    """
+    Opens a window for the user to view logs in.
+    """
+    LogWindow(root, "data/login_log.csv")
 
 
 def init_data_files():
+    """
+    If data files (members and logs) do not exist, creates them and writes appropriate headings.
+    """
     if not os.path.exists("data/"):
         os.makedirs("data/")
 
@@ -34,16 +39,13 @@ def init_data_files():
             f.write('name,name_lower,rfid_code,member_id,login_time,login_reason\n')
 
 
-def init_config():
-    global flight_circle_api_key, admin_rfid_id
-
-    config = ConfigHandler()
-
-    admin_rfid_id = int(config.get_config_element("DEFAULT/AdminRfidCode"))
-    flight_circle_api_key = int(config.get_config_element("DEFAULT/FlightCircleApiKey"))
-
-
-def save_member(rfid_id, name, member_id):
+def save_member(rfid_id, name, member_id=-1):
+    """
+    Saves a member to members_list.csv.
+    rfid_id: That member's rfid code
+    name: That member's name
+    member_id: That member's flight circle id, if provided.
+    """
     global members
     with open("data/members_list.csv", "a", encoding="utf-8", newline="") as f:
         writer = csv.writer(f)
@@ -52,30 +54,28 @@ def save_member(rfid_id, name, member_id):
 
 
 def close_add_member_window():
-    selection_index = search_results.curselection()
-    if not selection_index:
+    """
+    Prevents closing of add_member window if not all fields are full. Otherwise, saves member.
+    """
+    first_name = first_name_entry.get()
+    last_name = last_name_entry.get()
+
+    if not first_name or not last_name:
         return
-    name = search_results.get(selection_index)
+    name = f"{first_name} {last_name}"
 
     rfid_id = rfid_member_entry.get()
-    if len(rfid_id) < 2 or not rfid_id.isdigit():
+    if len(rfid_id) < 10 or not rfid_id.isdigit():
         return
 
     save_member(rfid_id, name, -1)
     add_member_window.destroy()
 
 
-def search_for_member():
-    first_name = first_name_entry.get()
-    last_name = last_name_entry.get()
-
-    search_results.delete(0, tk.END)  # Clear previous results
-    search_results.insert(tk.END, f"{first_name} {last_name}")
-    # search_results.insert(tk.END, "Result 2")
-    # search_results.insert(tk.END, "Result 3")
-
-
 def open_add_member_window():
+    """
+    Opens window to allow entry of new member information.
+    """
     global add_member_window
     add_member_window = ctk.CTkToplevel(root)
     add_member_window.title("Add Member")
@@ -92,13 +92,6 @@ def open_add_member_window():
     last_name_entry = ctk.CTkEntry(add_member_window)
     last_name_entry.pack()
 
-    search_button = ctk.CTkButton(add_member_window, text="Search for Member", command=search_for_member)
-    search_button.pack(pady=10)
-
-    global search_results
-    search_results = tk.Listbox(add_member_window, height=5, width=30)
-    search_results.pack(padx=10)
-
     global rfid_member_entry
     rfid_label = ctk.CTkLabel(add_member_window, text="Scan RFID Tag:")
     rfid_label.pack()
@@ -108,7 +101,6 @@ def open_add_member_window():
     add_member_button = ctk.CTkButton(add_member_window, text="Add Member", command=close_add_member_window)
     add_member_button.pack(pady=10)
 
-    # add_member_window.after(1000, add_member_window.lift())
     add_member_window.attributes('-topmost', True)
     add_member_window.update()
     add_member_window.attributes('-topmost', False)
@@ -116,13 +108,21 @@ def open_add_member_window():
 
 
 def open_empty_member_log_window(rfid_id):
+    """
+    Opens a window to ask for name of unknown member. Plays sound to let user know there was an error.
+    """
+    # Sleeps to let scanner sound play
+    time.sleep(0.2)
+    # Makes windows error noise to get user's attention
+    winsound.PlaySound("SystemHand", winsound.SND_ALIAS | winsound.SND_ASYNC)
+
     def save_empty_member_entry(event=None):
+        """
+        Saves unknown member. If name not present, saved as "unknown".
+        """
         name = unknown_member_name_entry.get()
 
-        # open_request_reason_window(rfid_id, name if name else "unknown", -1)
         save_entry(rfid_id, name if name else "unknown", reason="general")
-
-        # save_entry(rfid_id, name if name else None)
         empty_member_window.destroy()
 
     empty_member_window = ctk.CTkToplevel(root)
@@ -147,76 +147,82 @@ def open_empty_member_log_window(rfid_id):
     empty_member_window.focus()
 
 
-def open_request_reason_window(rfid_id, member_name, member_id):
-    def save_entry_with_reason():
-        selection_index = reason_option.curselection()
-        reason = ""
-        if not selection_index:
-            reason = "unknown"
-        else:
-            reason = reason_option.get(selection_index).lower()
-        save_entry(rfid_id, member_name, member_id, reason)
-        reason_request_window.destroy()
+# def open_request_reason_window(rfid_id, member_name, member_id):
+#     def save_entry_with_reason():
+#         selection_index = reason_option.curselection()
+#         reason = ""
+#         if not selection_index:
+#             reason = "unknown"
+#         else:
+#             reason = reason_option.get(selection_index).lower()
+#         save_entry(rfid_id, member_name, member_id, reason)
+#         reason_request_window.destroy()
 
-    reason_request_window = ctk.CTkToplevel(root)
-    reason_request_window.title("Input reason")
+#     reason_request_window = ctk.CTkToplevel(root)
+#     reason_request_window.title("Input reason")
 
-    instruction_label = ctk.CTkLabel(reason_request_window, text="Please select reason for login:")
-    instruction_label.pack(padx=20, pady=10)
+#     instruction_label = ctk.CTkLabel(reason_request_window, text="Please select reason for login:")
+#     instruction_label.pack(padx=20, pady=10)
 
-    reasons = [
-        "Work to fly",
-        "Volunteering - general",
-        "Flight lessons/study",
-        "CFI",
-        "Genral clubhouse use - nonflying",
-        "Ground school",
-        "Hangar talk/Monthy meeting",
-        "Visitor",
-        "Event",
-        "Other"
-    ]
+#     reasons = [
+#         "Work to fly",
+#         "Volunteering - general",
+#         "Flight lessons/study",
+#         "CFI",
+#         "Genral clubhouse use - nonflying",
+#         "Ground school",
+#         "Hangar talk/Monthy meeting",
+#         "Visitor",
+#         "Event",
+#         "Other"
+#     ]
 
-    reason_option = tk.Listbox(reason_request_window, height=len(reasons), width=30)
-    reason_option.pack(padx=10)
+#     reason_option = tk.Listbox(reason_request_window, height=len(reasons), width=30)
+#     reason_option.pack(padx=10)
 
-    reason_option.delete(0, tk.END)  # Clear previous results
-    for reason in reasons:
-        reason_option.insert(tk.END, reason)
+#     reason_option.delete(0, tk.END)  # Clear previous results
+#     for reason in reasons:
+#         reason_option.insert(tk.END, reason)
 
-    reason_option.bind("<Return>", save_entry_with_reason)
+#     reason_option.bind("<Return>", save_entry_with_reason)
 
-    log_button = ctk.CTkButton(reason_request_window, text="Save", command=save_entry_with_reason)
-    log_button.pack(pady=10)
+#     log_button = ctk.CTkButton(reason_request_window, text="Save", command=save_entry_with_reason)
+#     log_button.pack(pady=10)
 
-    reason_request_window.protocol("WM_DELETE_WINDOW", save_entry_with_reason)
-    reason_request_window.attributes('-topmost', True)
-    reason_request_window.update()
-    reason_request_window.attributes('-topmost', False)
-    reason_request_window.focus()
+#     reason_request_window.protocol("WM_DELETE_WINDOW", save_entry_with_reason)
+#     reason_request_window.attributes('-topmost', True)
+#     reason_request_window.update()
+#     reason_request_window.attributes('-topmost', False)
+#     reason_request_window.focus()
 
 
 def log_entry(event=None, rfid_id=None):
-    # if rfid_id is None:
-    #     rfid_id = rfid_entry.get()
+    """
+    Enters an entry into the treeview. Finds member from rfid_id, and prompts for name if not found.
+    """
     if len(rfid_id) >= 2 and rfid_id.isdigit():
         member = members.loc[members["rfid_code"] == int(rfid_id)]
 
-        if member.empty or int(rfid_id) == admin_rfid_id:
+        # If no member is found based on RFID code
+        if member.empty:
             open_empty_member_log_window(rfid_id)
         else:
             member_name = member["name"].iloc[0]
             member_id = member["member_id"].iloc[0]
 
+            # All members are assumed to be logging in for general reasons. People who are in the work to fly program
+            # have a special scanner, and their name has " (work)" added onto the end. This detects that.
             if "(work)" in member_name:
-                member_name = member_name.replace(" (work)", "")
+                member_name = member_name.replace("(work)", "").strip()
                 save_entry(rfid_id, member_name, reason="work to fly")
             else:
                 save_entry(rfid_id, member_name, reason="general")
-                # open_request_reason_window(rfid_id, member_name, member_id)
 
 
 def save_entry(rfid_id, member_name=None, member_id=-1, reason="unknown"):
+    """
+    Saves an entry into the log file and adds it to the treeview.
+    """
     if member_name is None:
         member_name = "UNKNOWN"
 
@@ -231,16 +237,20 @@ def save_entry(rfid_id, member_name=None, member_id=-1, reason="unknown"):
 
 
 currently_pressed_keys = ""
-# TODO verify this is the length of rfid codes
-
 rfid_code_length = 10
 
 def on_key_press(key):
+    """
+    Callback for any pressed key.
+    """
     global currently_pressed_keys
 
     try:
         key = key.char
 
+        # Clears recently pressed keys if a letter is pressed. This prevents program from building
+        # up numbers from random presses and then trying to save them. We will only try to save an 
+        # rfid code if 10 numbers and then enter are pressed consecutively.
         if key.isalpha():
             currently_pressed_keys = ""
 
@@ -251,44 +261,26 @@ def on_key_press(key):
             currently_pressed_keys = currently_pressed_keys[1:]
         # print(currently_pressed_keys)
     except AttributeError:
+        # This key is a special character. If it is the enter key and we have enough numbers stored, save it.
         if key == pynput.keyboard.Key.enter and len(currently_pressed_keys) == rfid_code_length:
             log_entry(rfid_id=currently_pressed_keys)
-            currently_pressed_keys = ""
+        # Always clear pressed keys on special character press.
+        currently_pressed_keys = ""
 
 
 listener = pynput.keyboard.Listener(on_press=on_key_press)
 listener.start()
 
-admin_rfid_id = None
-flight_circle_api_key = None
-
 init_data_files()
-init_config()
 
 members = pd.read_csv("data/members_list.csv")
 
 ctk.set_appearance_mode("light")
 root = ctk.CTk()
 root.title("Flight Club Sign In")
-# root.protocol("WM_DELETE_WINDOW", close_main_window)
-# root.geometry("500x400")
 
 icon = tk.PhotoImage(file="fc_502_logo.png")
 root.iconphoto(False, icon)
-
-# rfid_frame = ctk.CTkFrame(root)
-# rfid_frame.pack(side=tk.TOP, padx=10, pady=10)
-#
-# rfid_label = ctk.CTkLabel(rfid_frame, text="RFID ID")
-# rfid_label.pack(side=tk.LEFT, padx=(5, 0))
-#
-# rfid_entry = ctk.CTkEntry(rfid_frame, width=150)
-# rfid_entry.pack(side=tk.LEFT, padx=5)
-#
-# rfid_entry.bind('<Return>', log_entry)
-#
-# enter_button = ctk.CTkButton(rfid_frame, text="Enter", width=40, command=log_entry)
-# enter_button.pack(side=tk.LEFT)
 
 view_log_button = ctk.CTkButton(root, text="View Log", command=open_log_window)
 view_log_button.pack(pady=10)
@@ -297,9 +289,7 @@ table_frame = ctk.CTkFrame(root)
 table_frame.pack(padx=10, pady=10)
 
 style = ttk.Style()
-# style.theme_use('clam')
 style.configure("Treeview.Heading", font=(None, 16))
-# style.configure("Treeview.Entry", font=(None, 16))
 style.configure("Treeview", font=(None, 16), rowheight=30)
 
 columns = ("Member name", "Login time", "Login reason")
@@ -315,11 +305,3 @@ add_member_button = ctk.CTkButton(root, text="Add Member", command=open_add_memb
 add_member_button.pack(pady=10)
 
 root.mainloop()
-
-# while not exit_flag:
-#     print("test")
-#     if exit_flag:
-#         break
-
-# root.update_idletasks()
-# root.update()
